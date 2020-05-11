860d0ca07d2bd8d4426951b31363f3fba0721b75
Venkat Krishnan <myvvkmail@gmail.com>
2020-05-10 22:55:59 -0700

    Update ops.go

--- ops.go
+++ ops.go
@@ -15,8 +15,8 @@
 package fuse
 
 import (
-	"github.com/jacobsa/fuse/fuseops"
-	"github.com/jacobsa/fuse/internal/fusekernel"
+	"github.com/vrobot/fuse/fuseops"
+	"github.com/vrobot/fuse/internal/fusekernel"
 )
 
 // A sentinel used for unknown ops. The user is expected to respond with a
